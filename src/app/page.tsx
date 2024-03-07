1c52b34a1c25c85451e5b26e87921ce13258b036
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-06 19:57:46 -0700

    DREAM-9 Photo carousel component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 import Button from "@mui/material/Button";
+import PhotoCarousel from "@/components/PhotoCarousel";
 
 export default function Home() {
   return (
-      <p>Code is commented out</p>
-    /*<div>
-      Home Page<Button variant='outlined'>test</Button>
-      <p>both of the below elements are styled with tailwind classes</p>
-      <div className='bg-primary-main'>
-        my background is determined by theme.palette.primary.main. this color is
-        overriden in theme.ts
-      </div>
-      <div className='bg-error-main'>
-        my bg color is the default MUI error.main color
-      </div>
-    </div>*/
+    <>
+      <PhotoCarousel />
+    </>
   );
 }
