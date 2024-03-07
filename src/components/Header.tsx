bef25a6c71393c71cc438753a13e38aa2d8ba0c4
vbisso <valeriabisso19@gmail.com>
2024-03-06 18:19:05 -0700

    header and footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <h1>Header</h1>
-      <Link href='/'>Home</Link>
-      <Link href='/about'>About</Link>
+      <div id="logo"><img alt="logo" src="favicon.png"></img></div>
+      <ul id="navigationList">
+        <li><a href="">Home</a></li>
+        <li><a href="">Services</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Blog</a></li>
+      </ul>
     </header>
   );
 }
