fbf89a535c8e12fe522407aa1aa00c487dbf65be
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-20 19:59:26 -0600

    DREAM-38 Services page complete
    - Add images
    - Mobile styling

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,12 +1,121 @@
 import React from "react";
 import Service from "@/components/Service";
 
+export interface ServiceProps {
+  img: string;
+  title: string;
+  whatWeOffer: string[];
+  AddOns: string[];
+  price: number;
+  slug: string;
+}
+
+const services: ServiceProps[] = [
+  {
+    img: "/services/birthday.jpeg",
+    title: "Birthday Event",
+    whatWeOffer: [
+      "15 people (including moms, parents, your own photographer etc)",
+      "20+ backdrops & rooms",
+      "2 hours (including your setup)",
+      "Chairs + tables",
+      "Plates, cups and table cloths",
+      "Color palette of your choice (Blue, Green, Yellow, Orange, Pink, Purple, White, Black, Red)",
+      "Exclusive use of DreamBox",
+    ],
+    AddOns: [
+      "Additional persons after 12, $10 per person",
+      "20+ backdrops & rooms",
+      "Cake smash $50",
+      "Confetti $10",
+      "Photographer extra $50 (8 edited photos)",
+      "A Dozen Helium Balloons $40",
+      "Games (musical chairs, pin tail on donkey, Hulu hoop, eye spy Photo Booth) $10",
+    ],
+    price: 100,
+    slug: "birthday-event",
+  },
+  {
+    img: "/services/wedding.jpeg",
+    title: "Wedding Event",
+    whatWeOffer: [
+      "50 people (including bride, groom, photographers etc)",
+      "30+ backdrops & rooms",
+      "5 hours (including your setup)",
+      "Chairs + tables",
+      "Plates, cups and table cloths",
+      "Color palette of your choice (Blue, Green, Yellow, Orange, Pink, Purple, White, Black, Red)",
+      "Exclusive use of DreamBox",
+    ],
+    AddOns: [
+      "Additional persons after 50, $20 per person",
+      "30+ backdrops & rooms",
+      "Cake smash $100",
+      "Confetti $20",
+      "Photographer extra $100 (15 edited photos)",
+      "A Dozen Helium Balloons $80",
+      "Games (musical chairs, pin tail on donkey, Hulu hoop, eye spy Photo Booth) $20",
+    ],
+    price: 200,
+    slug: "wedding-event",
+  },
+  {
+    img: "/services/corporate.jpeg",
+    title: "Corporate Event",
+    whatWeOffer: [
+      "100 people (including employees, managers, photographers etc)",
+      "40+ backdrops & rooms",
+      "8 hours (including your setup)",
+      "Chairs + tables",
+      "Plates, cups and table cloths",
+      "Color palette of your choice (Blue, Green, Yellow, Orange, Pink, Purple, White, Black, Red)",
+      "Exclusive use of DreamBox",
+    ],
+    AddOns: [
+      "Additional persons after 100, $30 per person",
+      "40+ backdrops & rooms",
+      "Cake smash $150",
+      "Confetti $30",
+      "Photographer extra $150 (20 edited photos)",
+      "A Dozen Helium Balloons $120",
+      "Games (musical chairs, pin tail on donkey, Hulu hoop, eye spy Photo Booth) $30",
+    ],
+    price: 300,
+    slug: "corporate-event",
+  },
+  {
+    img: "/services/anniversary.jpeg",
+    title: "Anniversary Event",
+    whatWeOffer: [
+      "25 people (including couple, photographers etc)",
+      "25+ backdrops & rooms",
+      "3 hours (including your setup)",
+      "Chairs + tables",
+      "Plates, cups and table cloths",
+      "Color palette of your choice (Blue, Green, Yellow, Orange, Pink, Purple, White, Black, Red)",
+      "Exclusive use of DreamBox",
+    ],
+    AddOns: [
+      "Additional persons after 25, $15 per person",
+      "25+ backdrops & rooms",
+      "Cake smash $75",
+      "Confetti $15",
+      "Photographer extra $75 (10 edited photos)",
+      "A Dozen Helium Balloons $60",
+      "Games (musical chairs, pin tail on donkey, Hulu hoop, eye spy Photo Booth) $15",
+    ],
+    price: 150,
+    slug: "anniversary-event",
+  },
+];
+
 const Page = () => {
-    return (
-        <div>
-            <Service />
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-semibold pt-2">Services</h1>
+      <Service services={services} />
+    </div>
+  );
 };
 
 export default Page;
--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,62 +1,53 @@
 import React from "react";
 import BookNow from "@/components/BookNow";
+import { ServiceProps } from "@/app/services/page";
+import Image from "next/image";
 
-const services: ServiceProps[] = [{
-    img: "https://placehold.co/600x400",
-    title: "Birthday Event",
-    whatWeOffer: ["15 people (including moms, parents, your own photographer etc)",
-        "20+ backdrops & rooms",
-        "2 hours (including your setup)",
-        "Chairs + tables",
-        "Plates, cups and table cloths",
-        "Color palette of your choice (Blue, Green, Yellow, Orange, Pink, Purple, White, Black, Red)",
-        "Exclusive use of DreamBox"],
-    AddOns: ["Additional persons after 12, $10 per person",
-        "20+ backdrops & rooms",
-        "Cake smash $50",
-        "Confetti $10",
-        "Photographer extra $50 (8 edited photos)",
-        "A Dozen Hellium Balloons $40",
-        "Games (musical chairs, pin tail on donkey, Hulu hoop, eye spy Photo Booth) $10"],
-    price: 100,
-    slug: "birthday-event",
-}];
-
-interface ServiceProps {
-    img: string,
-    title: string
-    whatWeOffer: string[]
-    AddOns: string[]
-    price: number
-    slug: string
-}
-
-const Service = () => {
-    return (
-        <div className="p-4 sm:p-16">
-            {services.map((service, index) => (
-                <div key={index} className="grid grid-rows-1 sm:grid-rows-none sm:grid-cols-2">
-                    <img src={service.img} alt={service.title} className="rounded-xl m-auto"/>
-                    <div className="flex flex-col text-center px-4 pt-2 sm:px-8 sm:pt-4 justify-between">
-                        <div>
-                            <h1>{service.title}</h1>
-                            {service.whatWeOffer.map((listItem, index) => (
-                                <ul key={index} className="list-disc text-left pl-4">
-                                    <li>{listItem}</li>
-                                </ul>
-                            ))}
-                        </div>
-                        <div className="flex flex-col justify-end">
-                            <h1 className="text-2xl font-bold text-right">$100</h1>
-                            <div className="flex justify-end">
-                                <BookNow slug={service.slug} small/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
+const Service = ({ services }: { services: ServiceProps[] }) => {
+  return (
+    <div className="p-4 sm:px-16 max-w-screen-xl mx-auto">
+      {services.map((service, index) => (
+        <div
+          key={index}
+          className="grid grid-rows-1 sm:grid-rows-none sm:grid-cols-2 mb-4 border-b-2 border-backgroundDark"
+        >
+          <div className="m-auto py-4">
+            <h1 className="text-2xl font-semibold mb-2 sm:hidden">
+              {service.title}
+            </h1>
+            <Image
+              priority={index === 0}
+              src={service.img}
+              alt={service.title}
+              className="rounded-xl"
+              width={600}
+              height={400}
+            />
+          </div>
+          <div className="flex flex-col text-center px-4 pt-2 sm:px-8 sm:pt-4 justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold mb-2 hidden sm:block">
+                {service.title}
+              </h1>
+              {service.whatWeOffer.map((listItem, index) => (
+                <ul key={index} className="list-disc text-left pl-4">
+                  <li>{listItem}</li>
+                </ul>
+              ))}
+            </div>
+            <div className="flex flex-col justify-end">
+              <h1 className="text-2xl font-bold text-right">
+                ${service.price}
+              </h1>
+              <div className="flex justify-end my-2">
+                <BookNow slug={service.slug} small />
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Service;
