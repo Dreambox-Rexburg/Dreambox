1c4e51330fd1a767c3a90bc9937e3b57ecab19bc
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2024-05-09 16:04:56 -0600

    DREAM-47 Desktop Header.tsx styled for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,17 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header>
-      <div id="logo">
-        <Link href="/">
-          <Image
-            alt="logo"
-            src="/favicon.png"
-            width={200}
-            height={200}
-            priority
-          />
-        </Link>
-      </div>
-      <ul id="navigationList">
+    <header className="bg-customPrimaryLight flex justify-between px-8 py-2">
+      <Link href="/">
+        <Image
+          alt="logo"
+          src="/favicon.png"
+          width={100}
+          height={100}
+          priority
+        />
+      </Link>
+      <ul className="flex gap-4 items-center">
         <li>
           <Link href="/">Home</Link>
         </li>
