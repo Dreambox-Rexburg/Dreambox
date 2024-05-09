df8e2da23a8ad4a4a8a2cef862480db85e4214cd
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2024-05-09 16:44:04 -0600

    DREAM-48 Mobile menu added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,44 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export default function Header() {
+  const [showNav, setShowNav] = useState(false);
+
+  const navData = [
+    { name: "Home", href: "/" },
+    { name: "Services", href: "/services" },
+    { name: "About", href: "/about" },
+    { name: "Blog", href: "/blog" },
+    { name: "Book", href: "/book" },
+  ];
+
   return (
-    <header className="bg-customPrimaryLight flex justify-between px-8 py-2">
-      <Link href="/">
-        <Image
-          alt="logo"
-          src="/favicon.png"
-          width={100}
-          height={100}
-          priority
+    <div>
+      <div
+        className="bg-customPrimaryLight px-8 py-2
+        grid grid-cols-2
+        lg:grid-cols-none lg:flex lg:justify-between"
+      >
+        <Link href="/">
+          <Image
+            alt="logo"
+            src="/favicon.png"
+            width={100}
+            height={100}
+            priority
+          />
+        </Link>
+
+        <Bars3Icon
+          className="size-6 justify-self-end self-center cursor-pointer"
+          onClick={() => setShowNav(!showNav)}
         />
-      </Link>
-      <ul className="flex gap-4 items-center">
+
+        {/*<ul className="flex gap-4 items-center">
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -26,7 +51,22 @@ export default function Header() {
         <li>
           <a href="">Blog</a>
         </li>
-      </ul>
-    </header>
+      </ul>*/}
+      </div>
+
+      {/*  Mobile Menu Items  */}
+      {showNav && (
+        <ul className="bg-customBackgroundDefault">
+          {navData.map((item, index) => (
+            <ul key={index} className="text-center">
+              <li className="py-1">
+                <Link href={item.href}>{item.name}</Link>
+              </li>
+              <hr className="border-black/20" />
+            </ul>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
