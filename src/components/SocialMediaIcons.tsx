207f0c81beceea006c5df094deffac203a24b3ab
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2024-05-16 13:59:03 -0600

    DREAM-51 Styling
    - Fixed SocialMediaIcons.tsx
    - Fixed Hamburger menu in Header.tsx

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -9,21 +9,21 @@ import Link from "next/link";
 
 const SocialMediaIcons = () => {
   return (
-    <div className="flex gap-4">
+    <div className="flex">
       <Link
         href="https://www.facebook.com/profile.php?id=100087282444247"
         target="_blank"
       >
-        <FontAwesomeIcon icon={faFacebook} className="w-8 h-8" />
+        <FontAwesomeIcon icon={faFacebook} className="w-8 h-8 m-2" />
       </Link>
       <Link href="https://www.instagram.com/dreambox_rexburg/" target="_blank">
-        <FontAwesomeIcon icon={faInstagram} className="w-8 h-8" />
+        <FontAwesomeIcon icon={faInstagram} className="w-8 h-8 m-2" />
       </Link>
       <Link
         href="https://www.tiktok.com/@dreambox_rexburg?lang=en"
         target="_blank"
       >
-        <FontAwesomeIcon icon={faTiktok} className="w-8 h-8" />
+        <FontAwesomeIcon icon={faTiktok} className="w-8 h-8 m-2" />
       </Link>
     </div>
   );
