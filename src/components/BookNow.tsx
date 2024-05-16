981888c3ed6e38a246044578e89332ec891a0826
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2024-05-15 23:07:07 -0600

    DREAM-49 Updated Footer.tsx

--- src/components/BookNow.tsx
+++ src/components/BookNow.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const BookNow = ({ slug, small }: { slug?: string; small?: boolean }) => {
   return (
     <button
-      className={`${small && "text-sm"} py-1.5 px-2 font-bold rounded-3xl border-2 bg-customPrimaryDark text-primary-contrastText hover:bg-primary-contrastText hover:text-customPrimaryDark hover:border-customPrimaryDark`}
+      className={`${small && "text-sm"} py-1.5 px-2 font-bold rounded-3xl border-2 bg-customPrimaryDefault text-white hover:border-customPrimaryDark`}
     >
       {slug ? <Link href={`services/${slug}`}>Book Now!</Link> : "Book Now!"}
     </button>
