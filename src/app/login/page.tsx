5e97286a5cae22fdbc30864e4a053a304fca9cb4
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2024-06-12 17:39:23 -0600

    DREAM-53 Saving progress

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,23 @@
+import React, {FormEvent} from "react";
+import {loginAction} from "@/app/login/actions";
+
 const Page = () => {
-    console.log("hello")
+    const submitHandler = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const form = e.currentTarget;
+        const formData = new FormData(form)
+        await loginAction(formData)
+    }
 
     return (
-        <div>
-            Login Page
-        </div>
+        <form onSubmit={submitHandler}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email"/>
+
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password"/>
+        </form>
     );
 };
 
